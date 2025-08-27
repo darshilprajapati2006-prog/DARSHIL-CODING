46e66c42fd7e83135233ccb1ada2ca97c7f1cb39
Darshil Prajapati <darshilprajapati2006@gmail.com>
2025-08-27 19:26:27 +0530

    First commit

---
+++ c/firstCprogram.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("hello darshil\nhow are you");
+    return 0;  
+}
+
---
+++ c/intx.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    printf("hello darshil\nhow are you\n");
+    int x;
+    x=13;
+    printf("%d",x);
+    printf("\n");
+    x=7;
+    printf("%d",x);
+        printf("\n");
+    x=x+3;
+    printf("%d",x);
+    return 0;  
+}
