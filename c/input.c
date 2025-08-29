d52159e4aa73296e309144dff3ad3900f7186f4a
Darshil Prajapati <darshilprajapati2006@gmail.com>
2025-08-29 19:30:48 +0530

    Added input.c programm

---
+++ c/input.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value of a and b:");
+    scanf("%d%d",&a,&b);
+    printf("Value of a is: %d\n",a);
+    printf("Value of b is: %d\n",b);
+    return 0;  
+}
