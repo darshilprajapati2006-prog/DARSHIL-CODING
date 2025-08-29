189123d651c9f4bb5d72f98bef35474995d7f73d
Darshil Prajapati <darshilprajapati2006@gmail.com>
2025-08-29 12:24:30 +0530

    Added Scanf.c program

---
+++ c/Scanf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float radius;
+    scanf("%f",&radius);
+    float pi=3.14;
+    float area=pi*radius*radius;
+    printf("Area of circle is:%f",area);
+    return 0;  
+}
