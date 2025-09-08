354a92fc038d458b70c8eb749463a140b28b2eda
Darshil Prajapati <darshilprajapati2006@gmail.com>
2025-09-09 00:37:13 +0530

    Added new python programs

---
+++ python/morePrint.py
@@ -0,0 +1,39 @@
+print('hey i am a \"good boy\"')
+print("hey",7,45,sep="~",end="007\n")
+print("hello darshil")
+
+a = complex(3,4)
+b = True
+c = "darshil"
+d = None
+print(a)
+print(b)
+a1 = 3
+print(a+a1)
+print("the type of a is",type(a))
+print("the type of b is",type(b))
+print("the type of c is",type(c))
+
+list1 = [1,2,3,4,5]
+print("the type of list1 is",type(list1))
+
+print(2+5)
+print(2*5)
+print(2/5)
+print(2//5)
+print(2%5)
+print(2**5)
+
+a = 5
+b = 7
+print("the value of",a,"+",7,"is:",a+b)
+print("the value of",a,"-",7,"is:",a-b)
+print("the value of",a,"*",7,"is:",a*b)
+print("the value of",a,"/",7,"is:",a/b)
+
+a = "2"
+b = "5"
+print(int(a)+int(b))
+
+a = input("enter your name : ")
+print("my name is",a)
