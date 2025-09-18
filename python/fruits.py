6ecd6239500469446c018d982ef138325f2cfaa5
Darshil Prajapati <darshilprajapati2006@gmail.com>
2025-09-18 18:18:26 +0530

    Added new all programs

---
+++ python/fruits.py
@@ -0,0 +1,17 @@
+fruits = []
+f1 = input("Enter fruit 1: ")
+f2 = input("Enter fruit 2: ")
+f3 = input("Enter fruit 3: ")
+f4 = input("Enter fruit 4: ")
+f5 = input("Enter fruit 5: ")
+f6 = input("Enter fruit 6: ")
+f7 = input("Enter fruit 7: ")
+fruits.append(f1)
+fruits.append(f2)
+fruits.append(f3)
+fruits.append(f4)
+fruits.append(f5)
+fruits.append(f6)
+fruits.append(f7)
+print(fruits)
+                 
---
+++ python/name.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+print(f"good morning, {name}")
+
+l1 = [1, 2, 3, 4, 5]
+l1.reverse()
+print(l1)
+l1.insert(4,6)
+print(l1)
+l1.remove(3)
+print(l1)
+l1.pop(1)
+print(l1)
+l1.append(7)
+print(l1)
