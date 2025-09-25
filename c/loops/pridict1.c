3e8deecc8b2b23f167a67de59962a387fc2b3c41
Darshil Prajapati <darshilprajapati2006@gmail.com>
2025-09-26 00:12:46 +0530

    added all new programs

---
+++ c/loops/pridict1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i=1;
+    printf("Printing first 10 natural numbers:\n");
+    while(i<=10){
+        printf("\n%d",i);
+        i++;
+    }
+    return 0;
+}
---
+++ c/loops/primeNo.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    int a = 0;
+    for(int i=2;i<=n-1;i++){
+        if(n%i==0){
+            a = 1;
+            break;
+        }
+    }
+    if(a==0){
+        printf("%d is a prime number",n);
+    }
+    else{
+        printf("%d is not a prime number",n);
+    }
+    return 0;
+}
