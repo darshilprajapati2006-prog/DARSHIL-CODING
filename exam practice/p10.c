720958c5176b65342cb049a596899931f405d26b
Darshil Prajapati <darshilprajapati2006@gmail.com>
2025-11-02 14:44:03 +0530

    added all programs

---
+++ exam practice/p10.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int arr[]={1,3,45,2454,543,765,3452,765};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
