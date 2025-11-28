3d9fc97c34235ed723ab356753080cb60b328aab
Darshil Prajapati <darshilprajapati2006@gmail.com>
2025-11-29 02:26:50 +0530

    added all new files

---
+++ c/recursion/factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int factorial(int n){
+    if (n==1 || n==0) return 1;
+    return n * factorial(n - 1);
+}
+
+int main(){
+    int n;
+    printf("enter a number : ");
+    scanf("%d",&n);
+    int x = factorial(n);
+    printf("%d\n", x);
+    return 0;
+}
