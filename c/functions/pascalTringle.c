08a77d578a41841b26c6778bbb9c94d3f11cee75
Darshil Prajapati <darshilprajapati2006@gmail.com>
2025-12-05 00:22:16 +0530

    added all new files

--- c/functions/pascalTringle.c
+++ c/functions/pascalTringle.c
@@ -17,7 +17,7 @@ int main(){
     // int ncr=combination(n,r);
     // printf("%d",ncr);
     for(int i=0;i<=n;i++){
-        for(int s=0; s < n - i; s++){
+        for(int s=0; s <=n - i; s++){
             printf(" ");
         }
         for(int j=0;j<=i;j++){
