6296f2b3d2c0022dbe9fc24d778ca93a172bf598
Darshil Prajapati <darshilprajapati2006@gmail.com>
2025-12-12 13:02:06 +0530

    added all new files

---
+++ exam practice/p4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    typedef struct cricketer{
+        char name[20];
+        int age;
+        int matches;
+        float avg;
+    }cricketer;
+    cricketer arr[1];
+    int n = 1;
+    for(int i=0;i<n;i++){
+        scanf("%s", arr[i].name);
+        scanf("%d", &arr[i].age);
+        scanf("%d", &arr[i].matches);
+        scanf("%f", &arr[i].avg);
+    }
+    for(int i=0;i<n;i++){
+        printf("name : %s\n", arr[i].name);
+        printf("age : %d\n", arr[i].age);
+        printf("matches : %d\n", arr[i].matches);
+        printf("avg : %f\n", arr[i].avg);
+    }
+   return 0; 
+}
