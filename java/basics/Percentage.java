441368ea7b9396dbe16666f6c45924110162883a
Darshil Prajapati <darshilprajapati2006@gmail.com>
2026-01-08 08:58:59 +0530

    added new all files

---
+++ java/basics/Percentage.java
@@ -0,0 +1,12 @@
+public class Percentage {
+    public static void main(String[] args) {
+        int x1=80;
+        int x2=89;
+        int x3=84;
+        int x4=94;
+        int x5=79;
+        float z =(x1+x2+x3+x4+x5)/5;
+        System.out.println(z);
+    }
+    
+}
---
+++ java/basics/Sum2No.java
@@ -0,0 +1,7 @@
+public class Sum2No {
+    public static void main(String[] args) {
+        int x=4,y=3;
+        int z=x+y;
+        System.out.println("sum of two no : "+z);
+    }
+}
