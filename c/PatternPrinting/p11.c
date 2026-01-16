7c8ab3997fc58075848a3c9d5d030730cc01e665
Darshil Prajapati <darshilprajapati2006@gmail.com>
2026-01-16 08:27:56 +0530

    added all new files'

--- c/PatternPrinting/p11.c
+++ c/PatternPrinting/p11.c
@@ -4,6 +4,10 @@ int main(){
     printf("enter n : ");
     scanf("%d",&n);
     int m=(n/2)+1;
+    if(n%2==0) {
+        printf("not allow");
+        return 0;
+    }
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             if((i==m) || (j==m)){
