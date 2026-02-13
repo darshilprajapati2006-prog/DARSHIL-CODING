10e1aa76f4ad8465c7d80a7e161f227fac77e08b
Darshil Prajapati <darshilprajapati2006@gmail.com>
2026-02-13 10:39:56 +0530

    added all new files

---
+++ DSA/Linked_List/demo.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* next;
+    Node(int a){
+        val=a;
+    }
+    
+};
+int main(){
+    Node a(10);
+    Node b(20);
+    Node c(30);
+    Node d(40);
+    
+    a.next=&b;
+    b.next=&c;
+    c.next=&d;
+    d.next=NULL;
+
+    cout<<a.val;
+}
