4b1e72213eb706635231553992f089a2aa009a60
Darshil Prajapati <darshilprajapati2006@gmail.com>
2026-05-23 10:04:01 +0530

    added all new files

---
+++ DSA/TREES/postorder.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+    class node{
+            public:
+            int data;
+            node* left;
+            node* right;
+            node(int val){
+                data=val;
+                left=NULL;
+                right=NULL;
+            }
+    };
+    
+    void postorder(node* root){
+        if(root==NULL){
+            return;
+        }
+        postorder(root->left);
+        postorder(root->right);
+        cout<<root->data<<" ";
+    }
+    
+    int main(){
+        node* root=new node(1);
+        root->left=new node(2);
+        root->right=new node(3);
+        root->left->left=new node(4);
+        root->left->right=new node(5);
+        cout<<"Postorder Traversal: ";
+        postorder(root);
+        cout<<endl;
+        return 0;
+    }
---
+++ DSA/TREES/preorder.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+    class node{
+            public:
+            int data;
+            node* left;
+            node* right;
+            node(int val){
+                data=val;
+                left=NULL;
+                right=NULL;
+            }
+    };
+    
+    void preorder(node* root){
+        if(root==NULL){
+            return;
+        }
+        cout<<root->data<<" ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+    
+    int main(){
+        node* root=new node(1);
+        root->left=new node(2);
+        root->right=new node(3);
+        root->left->left=new node(4);
+        root->left->right=new node(5);
+        cout<<"Preorder Traversal: ";
+        preorder(root);
+        cout<<endl;
+        return 0;
+    }
